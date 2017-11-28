38b95a55dbedd0be5572118681030837eacf1ad3
ollestenborg <olle.stenborg@gmail.com>
2017-11-29 00:17:53 +0100

    Create formToObj.js

---
+++ formToObj.js
@@ -0,0 +1,11 @@
+export default () => {
+var eles=document.querySelectorAll(".criteria")
+console.log(eles)
+const hel= Array.from(eles).map((ele)=>{
+return Array.from(ele.children).map((field)=>{
+return field.value
+})
+console.log(criteriaArray)
+})
+return hel
+}
